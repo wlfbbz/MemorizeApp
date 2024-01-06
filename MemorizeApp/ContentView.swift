a6abf2dd4de740dcc08d5e559f214c570df9d3c3
wlfbbz <44901408+wlfbbz@users.noreply.github.com>
2024-01-06 01:54:36 +0000

    Initial Commit

---
+++ MemorizeApp/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  MemorizeApp
+//
+//  Created by Barbara on 05/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: Text {
+        VStack{
+            Text("hello")
+            Text("there")
+        }
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack(content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 25)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 25)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 25)
+            }
+        })
+    }
+}
+#Preview {
+    ContentView()
+}
---
+++ MemorizeApp/MemorizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeAppApp.swift
+//  MemorizeApp
+//
+//  Created by Barbara on 05/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
