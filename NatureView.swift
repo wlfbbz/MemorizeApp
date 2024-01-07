e3126c2a9fa6d6e43c2cc5928d749705d1c60ce3
wlfbbz <44901408+wlfbbz@users.noreply.github.com>
2024-01-07 07:49:44 +0000

    Week 2 Assignment

---
+++ NatureView.swift
@@ -0,0 +1,18 @@
+//
+//  NatureView.swift
+//  MemorizeApp
+//
+//  Created by Barbara on 07/01/2024.
+//
+
+import SwiftUI
+
+struct NatureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NatureView()
+}
